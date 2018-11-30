f202306cca5153cdcac5b2a478335399b19be835
Halil Can Ozcelik <hcanozcelik@gmail.com>
2018-11-30 23:39:04 +0300

    post added

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const http = require('http');
 
-/** yes, there is a related function for this request */
-function inquire(req, res) {
+/** handle GET request */
+function getHandler(req, res, reqUrl) {
     res.writeHead(200);
-    res.write('Yes, there is a response..\n');
+    res.write('GET parameters: ' + reqUrl.searchParams);
     res.end();
+}
+
+/** handle POST request */
+function postHandler(req, res, reqUrl) {
+    req.setEncoding('utf8');
+    req.on('data', (chunk) => {
+        res.writeHead(200);
+        res.write('POST parameters: ' + chunk);
+        res.end();
+    });
 }
 
 /** if there is no related function which handles the request, then show error message */
@@ -16,15 +26,16 @@ function noResponse(req, res) {
 
 http.createServer((req, res) => {
     // create an object for all redirection options
-    const funcRedirect = {
-        'GET/inquire': inquire,
+    const router = {
+        'GET/retrieve-data': getHandler,
+        'POST/send-data': postHandler,
         'default': noResponse
     };
     // parse the url by using WHATWG URL API
-    let reqUrl = new URL(req.url, 'http://127.0.0.1');
+    let reqUrl = new URL(req.url, 'http://127.0.0.1/');
     // find the related function by searching "method + pathname" and run it
-    let redirectedFunc = funcRedirect[req.method + reqUrl.pathname] || funcRedirect['default'];
-    redirectedFunc(req, res);
-}).listen(8080);
-
-console.log('Server is running at http://127.0.0.1:8080/');
+    let redirectedFunc = router[req.method + reqUrl.pathname] || router['default'];
+    redirectedFunc(req, res, reqUrl);
+}).listen(8080, () => {
+    console.log('Server is running at http://127.0.0.1:8080/');
+});
